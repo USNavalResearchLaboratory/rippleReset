e5104ff0e9028574ae4704258d532a99b25a1970
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-04 08:53:09 -0500

    Add gradient definitions

--- src/likelihoods.jl
+++ src/likelihoods.jl
@@ -10,6 +10,10 @@ function StatsBase.loglikelihood(::Type{NormalLikelihood},Y,μ)
     -0.5 * sum(abs2,Y .- μ)    
 end
 
+function gradient(::Type{NormalLikelihood},Y,μ)
+    -(Y .- μ)
+end
+
 struct PoissonLikelihood <: Likelihood
 end
 
@@ -17,11 +21,19 @@ function StatsBase.loglikelihood(::Type{PoissonLikelihood},Y,μ)
     dot(Y,log.(μ)) - sum(μ)
 end
 
+function gradient(::Type{PoissonLikelihood},Y,μ)
+    Y./μ .- 1
+end
+
 struct ExponentialLikelihood <: Likelihood
 end
 
 function StatsBase.loglikelihood(::Type{ExponentialLikelihood},Y,μ)
     -sum(Y ./ μ) - sum(log,μ)
+end
+
+function gradient(::Type{ExponentialLikelihood},Y,μ)
+    Y ./ abs2.(μ) - inv.(μ)
 end
 
 
