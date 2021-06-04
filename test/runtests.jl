d98c4f3b36477416cb69ded31c26830b59028387
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-04 13:53:28 -0500

    Add likelihood and link function evaluations to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,11 +13,18 @@ X = randn(N,D)
 ## Normal data
 Y1 = rand.(Normal.(μ,1.0))
 
+loglikelihood(NormalLikelihood,Y1,link(IdentityLink,μ))
+
 ## Poisson data
 Y2 = rand.(Poisson.(exp.(μ)))
 
+loglikelihood(PoissonLikelihood,Y2,link(LogLink,μ))
+
 ## Exponential data
 Y3 = rand.(Exponential.(exp.(μ)))
+
+loglikelihood(ExponentialLikelihood,Y3,link(LogLink,μ))
+
 
 m1 = fit(KRRModel,RBFKernel(1.0),NormalLikelihood,IdentityLink,X,Y1,1.0,1.0,verbose=true)
 m2 = fit(KRRModel,RBFKernel(1.0),PoissonLikelihood,LogLink,X,Y2,1.0,1.0,verbose=true,rank=N-1)
