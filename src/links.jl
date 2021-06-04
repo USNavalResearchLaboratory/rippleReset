dc0cc0e58810474fa5fee60b7ab0ae434a738999
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-04 14:05:05 -0500

    Extend Base.inv for link function inverses

--- src/links.jl
+++ src/links.jl
@@ -10,7 +10,7 @@ end
 link(::Type{IdentityLink},η) = η
 
 # This computes g(μ)
-inv(::Type{IdentityLink},μ) = μ
+Base.inv(::Type{IdentityLink},μ) = μ
 
 gradient(::Type{IdentityLink},η) = ones(eltype(η),length(η))
 hessian(::Type{IdentityLink},η)  = zeros(eltype(η),length(η))
@@ -19,7 +19,7 @@ struct LogLink <: Link
 end
 
 link(::Type{LogLink},η) = exp.(η)
-inv(::Type{LogLink},μ)  = log.(μ)
+Base.inv(::Type{LogLink},μ)  = log.(μ)
 
 gradient(::Type{LogLink},η) = exp.(η)
 hessian(::Type{LogLink},η)  = exp.(η)
@@ -28,7 +28,7 @@ struct LogitLink <: Link
 end
 
 link(::Type{LogitLink},η) = 1 ./ (1 .+ exp.(η))
-inv(::Type{LogitLink},μ) = log.(μ ./ (1-μ))
+Base.inv(::Type{LogitLink},μ) = log.(μ ./ (1-μ))
 
 gradient(::Type{LogitLink},η) = link(LogitLink,η) .* (1 .- link(LogitLink,η))
 hessian(::Type{LogitLink},η) = gradient(LogitLink,η) .* (1 .- 2 .* link(LogitLink,η))
