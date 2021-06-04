9553e63ced65a718180f770a6e38c12dfb768769
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-04 14:45:54 -0500

    Add tests for KRRModel likelihood gradient API

--- test/runtests.jl
+++ test/runtests.jl
@@ -28,6 +28,10 @@ X = randn(N,D)
 
         @test KRR.loglikelihood(KRRModel,NormalLikelihood,IdentityLink,Y1,X,β) ≈ l1
 
+        g1 = KRR.gradient(KRRModel,NormalLikelihood,IdentityLink,Y1,X,β)        
+        
+        @test g1 ≈ X'*(Y1 .- μ1)
+
         m1 = fit(KRRModel,RBFKernel(1.0),NormalLikelihood,IdentityLink,X,Y1,1.0,1.0,verbose=true)
     end
 
@@ -45,6 +49,10 @@ X = randn(N,D)
 
         @test KRR.loglikelihood(KRRModel,PoissonLikelihood,LogLink,Y2,X,β) ≈ l2
 
+        g2 = KRR.gradient(KRRModel,PoissonLikelihood,LogLink,Y2,X,β)
+
+        @test g2 ≈ X'*(Y2 .- μ2)
+
         m2 = fit(KRRModel,RBFKernel(1.0),PoissonLikelihood,LogLink,X,Y2,1.0,1.0,verbose=true,rank=N-1)
     end
 
@@ -59,6 +67,10 @@ X = randn(N,D)
         @test KRR.hessian(ExponentialLikelihood,Y3,μ3) ≈ inv.(abs2.(μ3)) .- 2 .* Y3 ./ μ3.^3
 
         @test KRR.loglikelihood(KRRModel,ExponentialLikelihood,LogLink,Y3,X,β) ≈ l3
+
+        g3 = KRR.gradient(KRRModel,ExponentialLikelihood,LogLink,Y3,X,β)
+
+        @test g3 ≈ X'*(Y3./μ3 .- 1)
 
         m3 = fit(KRRModel,RBFKernel(1.0),ExponentialLikelihood,LogLink,X,Y3,1.0,1.0,verbose=true)
     end
