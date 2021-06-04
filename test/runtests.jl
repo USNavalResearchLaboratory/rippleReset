863ba5421354821bc8953668973eccf259f9e3bd
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-04 14:17:41 -0500

    Add hessian tests and nest testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,36 +12,50 @@ X = randn(N,D)
 β = randn(D)
 η = X * β
 
-@testset "Normal data" begin
-    μ1 = link(IdentityLink,η)
-    Y1 = rand.(Normal.(μ1,1.0))
+@testset "KRR" begin
 
-    KRR.loglikelihood(NormalLikelihood,Y1,μ1)
+    @testset "Likelihood and link evaluations" begin
+        
+        @testset "Normal data" begin
+            μ1 = link(IdentityLink,η)
+            Y1 = rand.(Normal.(μ1,1.0))
 
-    @test KRR.gradient(NormalLikelihood,Y1,μ1) ≈ (Y1 .- μ1)
+            KRR.loglikelihood(NormalLikelihood,Y1,μ1)
+
+            @test KRR.gradient(NormalLikelihood,Y1,μ1) ≈ (Y1 .- μ1)
+
+            @test KRR.hessian(NormalLikelihood,Y1,μ1) ≈ -ones(N)
+        end
+
+        @testset "Poisson data" begin
+            μ2 = link(LogLink,η)
+            Y2 = rand.(Poisson.(μ2))
+
+            KRR.loglikelihood(PoissonLikelihood,Y2,μ2)
+
+            @test KRR.gradient(PoissonLikelihood,Y2,μ2) ≈ Y2 ./ μ2 .- 1
+
+            @test KRR.hessian(PoissonLikelihood,Y2,μ2) ≈ -Y2 ./ abs2.(μ2)
+        end
+
+        @testset "Exponential data" begin
+            μ3 = link(LogLink,η)
+            Y3 = rand.(Exponential.(μ3))
+
+            KRR.loglikelihood(ExponentialLikelihood,Y3,μ3)
+
+            @test KRR.gradient(ExponentialLikelihood,Y3,μ3) ≈ Y3 ./ abs2.(μ3) .- inv.(μ3)
+
+            @test KRR.hessian(ExponentialLikelihood,Y3,μ3) ≈ inv.(abs2.(μ3)) .- 2 .* Y3 ./ μ3.^3
+        end
+    end
+
+    @testset "Model fitting" begin
+        m1 = fit(KRRModel,RBFKernel(1.0),NormalLikelihood,IdentityLink,X,Y1,1.0,1.0,verbose=true)
+        m2 = fit(KRRModel,RBFKernel(1.0),PoissonLikelihood,LogLink,X,Y2,1.0,1.0,verbose=true,rank=N-1)
+        m3 = fit(KRRModel,RBFKernel(1.0),ExponentialLikelihood,LogLink,X,Y3,1.0,1.0,verbose=true)
+    end
 end
-
-@testset "Poisson data" begin
-    μ2 = link(LogLink,η)
-    Y2 = rand.(Poisson.(μ2))
-
-    KRR.loglikelihood(PoissonLikelihood,Y2,μ2)
-
-    @test KRR.gradient(PoissonLikelihood,Y2,μ2) ≈ Y2 ./ μ2 .- 1
-end
-
-@testset "Exponential data" begin
-    μ3 = link(LogLink,η)
-    Y3 = rand.(Exponential.(μ3))
-
-    KRR.loglikelihood(ExponentialLikelihood,Y3,μ3)
-
-    @test KRR.gradient(ExponentialLikelihood,Y3,μ3) ≈ Y3 ./ abs2.(μ3) .- inv.(μ3)
-end
-
-m1 = fit(KRRModel,RBFKernel(1.0),NormalLikelihood,IdentityLink,X,Y1,1.0,1.0,verbose=true)
-m2 = fit(KRRModel,RBFKernel(1.0),PoissonLikelihood,LogLink,X,Y2,1.0,1.0,verbose=true,rank=N-1)
-m3 = fit(KRRModel,RBFKernel(1.0),ExponentialLikelihood,LogLink,X,Y3,1.0,1.0,verbose=true)
 
 
 
