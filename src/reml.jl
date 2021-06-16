1e1de99836190a89c2050cfbd04c16dda633c56d
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2021-06-16 10:08:01 -0500

    Use RankChoice API for REML fitting

--- src/reml.jl
+++ src/reml.jl
@@ -1,6 +1,7 @@
 function StatsBase.fit(::Type{KRRModel},::Type{LikelihoodType},::Type{LinkType},
                        X,Y,
-                       Δ;
+                       Δ,
+                       rankchoice::RankChoice;
                        verbose=false,
                        kwargs...) where {LikelihoodType <: Likelihood, LinkType <: Link}
 
@@ -11,10 +12,11 @@ function StatsBase.fit(::Type{KRRModel},::Type{LikelihoodType},::Type{LinkType},
                      RBFKernel(exp(θ[1])),
                      X,Y,
                      exp(θ[2]),
-                     Δ;
-                     kwargs...)) - θ[1] + 2*exp(θ[1])/L0 - θ[2] + 2*exp(θ[2])/γ0
+                     Δ,
+                     rankchoice;
+                     kwargs...))
 
-    opt = optimize(f,log.([L0/2;γ0/2]),NelderMead(),Optim.Options(show_trace=verbose))
+    opt = optimize(f,log.([L0;γ0]),NelderMead(),Optim.Options(show_trace=verbose))
     θn = Optim.minimizer(opt)
-    fit(KRRModel,LikelihoodType,LinkType,RBFKernel(exp(θn[1])),X,Y,exp(θn[2]),Δ;kwargs...)                    
+    fit(KRRModel,LikelihoodType,LinkType,RBFKernel(exp(θn[1])),X,Y,exp(θn[2]),Δ,rankchoice;kwargs...)                    
 end
