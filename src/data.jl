e241c82e7b22620b2c553058d940f29bd05d396f
William Kearney <William.Kearney.ctr@nrlssc.navy.mil>
2022-11-17 03:30:48 -0600

    Add rudimentary data loading functionality

---
+++ src/data.jl
@@ -0,0 +1,12 @@
+"""
+    read_forcing(forcing_file)
+
+Read a RippleReset forcing file and return the ripple reset number and the
+resets.
+"""
+function read_forcing(forcing_file)
+    D = CSV.read(forcing_file,DataFrame)
+    Λ = D[!,:Λ]
+    Y = D[!,:reset2]
+    Λ,Y
+end
